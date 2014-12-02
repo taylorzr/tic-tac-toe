2efc65f03cacb76a260d812d4659a16b7c93fc1c
Zach Taylor <taylorzr@gmail.com>
2014-12-02 03:07:44 -0600

    Adds finding best cell with tests, and small refactors

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -20,37 +20,84 @@ describe("Game", function() {
     });
   });
 
-  describe("#rowForCellIndex", function() {
+  describe("#bestCellFor", function() {
+    it ("should return the middle cell if the board is empty", function() {
+      expect(game.bestCellFor("x")).toEqual(4);
+    });
+  });
+
+  describe("#rowOpenFor", function() {
+    it ("should return true when the row has no opponent values", function() {
+      game.mark("x", 3);
+      expect(game.rowOpenFor("x", 0)).toBeTruthy();
+    });
+
+    it ("should return false when the row has opponent values", function() {
+      game.mark("o", 3);
+      expect(game.rowOpenFor("x", 0)).toBeFalsy();
+    });
+  });
+
+  describe("#columnOpenFor", function() {
+    it ("should return true when the column has no opponent values", function() {
+      game.mark("x", 7);
+      expect(game.columnOpenFor("x", 0)).toBeTruthy();
+    });
+
+    it ("should return false when the column has opponent values", function() {
+      game.mark("o", 7);
+      expect(game.columnOpenFor("x", 0)).toBeFalsy();
+    });
+  });
+
+  describe("#diagonalsOpenFor", function() {
+    it ("should return 2 when the diagonals have no opponent values", function() {
+      game.mark("x", 5);
+      expect(game.diagonalsOpenFor("x", 4)).toEqual(2);
+    });
+
+    it ("should return 1 when a diagonal has 1 opponent values", function() {
+      game.mark("o", 7);
+      expect(game.diagonalsOpenFor("x", 4)).toEqual(1);
+    });
+
+    it ("should return 0 when the cell isn't on a diagonal", function() {
+      game.mark("x", 2);
+      expect(game.diagonalsOpenFor("x", 1)).toEqual(0);
+    });
+  });
+
+  describe("#rowForCell", function() {
     it ("should return the row values for a given cell index", function() {
       game.mark("x", 1);
       game.mark("o", 2);
       game.mark("x", 3);
-      expect(game.rowForCellIndex(0)).toEqual(["x", "o", "x"]);
+      expect(game.rowForCell(0)).toEqual(["x", "o", "x"]);
     });
   });
 
-  describe("#columnForCellIndex", function() {
+  describe("#columnForCell", function() {
     it ("should return the column values for a given cell index", function() {
       game.mark("x", 1);
       game.mark("o", 4);
       game.mark("x", 7);
-      expect(game.columnForCellIndex(0)).toEqual(["x", "o", "x"]);
+      expect(game.columnForCell(0)).toEqual(["x", "o", "x"]);
     });
   });
 
-  describe("#diagonalsForCellIndex", function() {
+  describe("#diagonalsForCell", function() {
     it ("should return the \\ diagonal values for a \\ diagonal cell index", function() {
       game.mark("x", 1);
       game.mark("o", 5);
       game.mark("x", 9);
-      expect(game.diagonalsForCellIndex(0)).toEqual([xox, []]);
+      expect(game.diagonalsForCell(0)).toEqual([xox, null]);
     });
 
     it ("should return the / diagonal values for a / diagonal cell index", function() {
       game.mark("x", 3);
       game.mark("o", 5);
       game.mark("x", 7);
-      expect(game.diagonalsForCellIndex(2)).toEqual([[], xox]);
+      expect(game.diagonalsForCell(2)).toEqual([null, xox]);
     })
 
     it ("should return both the \\ and / diagonal values for the middle index", function() {
@@ -60,11 +107,11 @@ describe("Game", function() {
       game.mark("x", 3);
       game.mark("o", 5);
       game.mark("x", 7);
-      expect(game.diagonalsForCellIndex(4)).toEqual([xox, xox]);
+      expect(game.diagonalsForCell(4)).toEqual([xox, xox]);
     });
 
-    it ("should return two empty arrays for a non-diagonal cell index", function() {
-      expect(game.diagonalsForCellIndex(1)).toEqual([[], []]);
+    it ("should return two nulls for a non-diagonal cell index", function() {
+      expect(game.diagonalsForCell(1)).toEqual([null, null]);
     });
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,43 +1,83 @@
 function Game() {
   this.name = "Tic-Tac-Toe"; 
-  this.board = new Array(9);
+  this.board = Array.apply(null, Array(9)).map(function(){ return null; });
+  this.diagonal1 = [0, 4, 8];
+  this.diagonal2 = [2, 4, 6];
 }
 
 Game.prototype.mark = function(symbol, cell) {
   this.board[cell - 1] = symbol;
 }
 
-Game.prototype.rowForCellIndex = function(cellIndex) {
+Game.prototype.bestCellFor = function(symbol) {
+  var ratings = this.board.map(function(_, cellIndex) {
+    var rating = 0;
+    if (this.rowOpenFor(symbol, cellIndex)) rating += 1;
+    if (this.columnOpenFor(symbol, cellIndex)) rating += 1;
+    rating += this.diagonalsOpenFor(symbol, cellIndex);
+    return rating;
+  }, this);
+  return ratings.indexOf(Math.max.apply(null, ratings));
+}
+
+Game.prototype.rowOpenFor = function(symbol, cellIndex) {
+  return this.rowForCell(cellIndex).every(function(cellValue) {
+    return cellValue == symbol || cellValue == null; 
+  });
+}
+
+Game.prototype.columnOpenFor = function(symbol, cellIndex) {
+  return this.columnForCell(cellIndex).every(function(cellValue) {
+    return cellValue == symbol || cellValue == null;
+  });
+}
+
+Game.prototype.diagonalsOpenFor = function(symbol, cellIndex) {
+  if (!this.cellOnDiagonal(cellIndex)) return 0; 
+  
+  var diagonalsValues = this.diagonalsForCell(cellIndex);
+  return diagonalsValues.reduce(function(count, diagonalValues) {
+    if (diagonalValues == null) return count;
+    var diagonalOpen = diagonalValues.every(function(cellValue) {
+      return cellValue == symbol || cellValue == null;
+    });
+    if (diagonalOpen) count += 1;
+    return count;
+  }, 0);
+}
+
+Game.prototype.rowForCell = function(cellIndex) {
   return this.board.filter(function(_, index) {
     return Math.floor(index / 3) == Math.floor(cellIndex / 3);
   });
 }
 
-Game.prototype.columnForCellIndex = function(cellIndex) {
+Game.prototype.columnForCell = function(cellIndex) {
   return this.board.filter(function(_, index) {
     return index % 3 == cellIndex % 3;
   });
 }
 
-Game.prototype.diagonalsForCellIndex = function(cellIndex) {
-  // cellIndex isn't on a diagonal if the index is odd
-  if (cellIndex % 2 == 1) return [[], []];
+Game.prototype.diagonalsForCell = function(cellIndex) {
+  if (!this.cellOnDiagonal(cellIndex)) return [null, null];
   
-  var diagonalValues1 = [];
-  var diagonalIndexes1 = [0, 4, 8];
-  if (diagonalIndexes1.indexOf(cellIndex) != -1) {
+  var diagonalValues1 = null;
+  if (this.diagonal1.indexOf(cellIndex) != -1) {
     diagonalValues1 = this.board.filter(function(_, index) {
-      return [0, 4, 8].indexOf(index) != -1;
-    });
+      return this.diagonal1.indexOf(index) != -1;
+    }, this);
   }
 
-  var diagonalValues2 = [];
-  var diagonalIndexes2 = [2, 4, 6];
-  if (diagonalIndexes2.indexOf(cellIndex) != -1) {
+  var diagonalValues2 = null;
+  if (this.diagonal2.indexOf(cellIndex) != -1) {
     diagonalValues2 = this.board.filter(function(_, index) {
-      return diagonalIndexes2.indexOf(index) != -1; 
-    });
+      return this.diagonal2.indexOf(index) != -1; 
+    }, this);
   }
 
   return [diagonalValues1, diagonalValues2];
+}
+
+Game.prototype.cellOnDiagonal = function(cellIndex) {
+  return cellIndex % 2 == 0;
 }
