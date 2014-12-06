d89372e59996d43bab8403fa90bfada8314328f2
Zach Taylor <taylorzr@gmail.com>
2014-12-05 18:20:50 -0600

    Refactors mark to use 0-index

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -15,7 +15,7 @@ describe("Game", function() {
 
   describe("#mark", function() {
     it ("should mark a cell", function() {
-      game.mark("x", 1);
+      game.mark("x", 0);
       expect(game.board[0]).toEqual("x");
     });
   });
@@ -36,20 +36,20 @@ describe("Game", function() {
     });
 
     it ("should return 0 for the center cell full blocked", function() {
-      game.mark("o", 4);
-      game.mark("o", 8);
-      game.mark("o", 1);
       game.mark("o", 3);
+      game.mark("o", 7);
+      game.mark("o", 0);
+      game.mark("o", 2);
       expect(game.openRatingFor("x", 4)).toEqual(0);
     });
   });
 
   describe("#blockRatingFor", function() {
     it ("should return 4 for the center cell blocking horizontal, vertical and diagonals", function() {
-      game.mark("o", 4);
-      game.mark("o", 8);
-      game.mark("o", 1);
       game.mark("o", 3);
+      game.mark("o", 7);
+      game.mark("o", 0);
+      game.mark("o", 2);
       expect(game.blockRatingFor("x", 4)).toEqual(4);
     });
 
@@ -58,37 +58,62 @@ describe("Game", function() {
     });
   });
 
+  // should return 2 if blocks win
   describe("#rowBlockFor", function() {
-    it ("should return 1 when the row has an opponent value", function() {
-      game.mark("o", 3);
+    it ("should return 2 when the row has 2 opponent values", function() {
+      game.mark("o", 1);
+      game.mark("o", 2);
+      expect(game.rowBlockFor("x", 0)).toEqual(2);
+    });
+    
+    it ("should return 1 when the row has 1 opponent value", function() {
+      game.mark("o", 2);
       expect(game.rowBlockFor("x", 0)).toEqual(1);
     });
 
     it ("should return 0 when the row has no opponent values", function() {
       expect(game.rowBlockFor("x", 0)).toEqual(0);
     });
+
+    it ("should return 0 when the row has an opponent value, but it is already blocked", function() {
+      game.mark("o", 1);
+      game.mark("x", 2);
+      expect(game.rowBlockFor("x", 0)).toEqual(0);
+    });
   });
 
   describe("#columnBlockFor", function() {
-    it ("should return 1 when the column has an opponent value", function() {
-      game.mark("o", 7);
+    it ("should return 2 when the column has 2 opponent values", function() {
+      game.mark("o", 3);
+      game.mark("o", 6);
+      expect(game.columnBlockFor("x", 0)).toEqual(2);
+    });
+
+    it ("should return 1 when the column has 1 opponent value", function() {
+      game.mark("o", 6);
       expect(game.columnBlockFor("x", 0)).toEqual(1);
     });
 
     it ("should return 0 when the column has no opponent values", function() {
       expect(game.columnBlockFor("x", 0)).toEqual(0);
     });
+
+    it ("should return 0 when the column has an opponent value, but it is already blocked", function() {
+      game.mark("o", 3);
+      game.mark("x", 6);
+      expect(game.rowBlockFor("x", 0)).toEqual(0);
+    });
   });
 
   describe("#diagonalsBlockFor", function() {
     it ("should return 2 when the both diagonals are blocked for the opponent", function() {
-      game.mark("o", 1);
-      game.mark("o", 3);
+      game.mark("o", 0);
+      game.mark("o", 2);
       expect(game.diagonalsBlockFor("x", 4)).toEqual(2);
     });
 
     it ("should return 1 when the 1 diagonal \\ is blocked for the opponent", function() {
-      game.mark("o", 1);
+      game.mark("o", 0);
       expect(game.diagonalsBlockFor("x", 4)).toEqual(1);
     });
 
@@ -100,85 +125,85 @@ describe("Game", function() {
 
   describe("#rowOpenFor", function() {
     it ("should return 1 when the row has no opponent values", function() {
-      game.mark("x", 3);
+      game.mark("x", 2);
       expect(game.rowOpenFor("x", 0)).toEqual(1);
     });
 
     it ("should return 0 when the row has opponent values", function() {
-      game.mark("o", 3);
+      game.mark("o", 2);
       expect(game.rowOpenFor("x", 0)).toEqual(0);
     });
   });
 
   describe("#columnOpenFor", function() {
     it ("should return 1 when the column has no opponent values", function() {
-      game.mark("x", 7);
+      game.mark("x", 6);
       expect(game.columnOpenFor("x", 0)).toBeTruthy();
     });
 
     it ("should return 0 when the column has opponent values", function() {
-      game.mark("o", 7);
+      game.mark("o", 6);
       expect(game.columnOpenFor("x", 0)).toBeFalsy();
     });
   });
 
   describe("#diagonalsOpenFor", function() {
     it ("should return 2 when the diagonals have no opponent values", function() {
-      game.mark("x", 5);
+      game.mark("x", 4);
       expect(game.diagonalsOpenFor("x", 4)).toEqual(2);
     });
 
     it ("should return 1 when a diagonal has 1 opponent values", function() {
-      game.mark("o", 7);
+      game.mark("o", 6);
       expect(game.diagonalsOpenFor("x", 4)).toEqual(1);
     });
 
     it ("should return 0 when the cell isn't on a diagonal", function() {
-      game.mark("x", 2);
+      game.mark("x", 1);
       expect(game.diagonalsOpenFor("x", 1)).toEqual(0);
     });
   });
 
   describe("#rowForCell", function() {
     it ("should return the row values for a given cell index", function() {
-      game.mark("x", 1);
-      game.mark("o", 2);
-      game.mark("x", 3);
+      game.mark("x", 0);
+      game.mark("o", 1);
+      game.mark("x", 2);
       expect(game.rowForCell(0)).toEqual(["x", "o", "x"]);
     });
   });
 
   describe("#columnForCell", function() {
     it ("should return the column values for a given cell index", function() {
-      game.mark("x", 1);
-      game.mark("o", 4);
-      game.mark("x", 7);
+      game.mark("x", 0);
+      game.mark("o", 3);
+      game.mark("x", 6);
       expect(game.columnForCell(0)).toEqual(["x", "o", "x"]);
     });
   });
 
   describe("#diagonalsForCell", function() {
     it ("should return the \\ diagonal values for a \\ diagonal cell index", function() {
-      game.mark("x", 1);
-      game.mark("o", 5);
-      game.mark("x", 9);
+      game.mark("x", 0);
+      game.mark("o", 4);
+      game.mark("x", 8);
       expect(game.diagonalsForCell(0)).toEqual([xox, null]);
     });
 
     it ("should return the / diagonal values for a / diagonal cell index", function() {
-      game.mark("x", 3);
-      game.mark("o", 5);
-      game.mark("x", 7);
+      game.mark("x", 2);
+      game.mark("o", 4);
+      game.mark("x", 6);
       expect(game.diagonalsForCell(2)).toEqual([null, xox]);
     })
 
     it ("should return both the \\ and / diagonal values for the middle index", function() {
-      game.mark("x", 1);
-      game.mark("o", 5);
-      game.mark("x", 9);
-      game.mark("x", 3);
-      game.mark("o", 5);
-      game.mark("x", 7);
+      game.mark("x", 0);
+      game.mark("o", 4);
+      game.mark("x", 8);
+      game.mark("x", 2);
+      game.mark("o", 4);
+      game.mark("x", 6);
       expect(game.diagonalsForCell(4)).toEqual([xox, xox]);
     });
 
