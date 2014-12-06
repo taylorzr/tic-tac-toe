3809d324e6996e5f128cac62dda8a8da2bc3bb12
Zach Taylor <taylorzr@gmail.com>
2014-12-05 18:08:05 -0600

    Adds better rating for open and block, and refactors open and block to use the same method for all lines

--- src/Game.js
+++ src/Game.js
@@ -36,71 +36,92 @@ Game.prototype.blockRatingFor = function(symbol, cellIndex) {
   return this.rowBlockFor(symbol, cellIndex) + this.columnBlockFor(symbol, cellIndex) + this.diagonalsBlockFor(symbol, cellIndex);
 }
 
-Game.prototype.rowBlockFor = function(symbol, cellIndex) {
-  var notAlreadyBlocked = this.rowForCell(cellIndex).every(function(cellValue){
+Game.prototype.lineBlockFor = function(symbol, lineValues) {
+  var notAlreadyBlocked = lineValues.every(function(cellValue){
     return cellValue != symbol;
   });
-  var blockCount = this.rowForCell(cellIndex).reduce(function(count, cellValue) {
+  var blockCount = lineValues.reduce(function(count, cellValue) {
     if (cellValue != symbol && cellValue != null) count += 1;
     return count;
   }, 0);
   return notAlreadyBlocked ? blockCount : 0;
 }
 
+Game.prototype.rowBlockFor = function(symbol, cellIndex) {
+  var rowValues = this.rowForCell(cellIndex);
+  return this.lineBlockFor(symbol, rowValues);
+}
+
 Game.prototype.columnBlockFor = function(symbol, cellIndex) {
-  var notAlreadyBlocked = this.columnForCell(cellIndex).every(function(cellValue){
-    return cellValue != symbol;
-  });
-  var blockCount = this.columnForCell(cellIndex).reduce(function(count, cellValue) {
-    if (cellValue != symbol && cellValue != null) count += 1;
-    return count;
-  }, 0);
-  return notAlreadyBlocked ? blockCount : 0;
+  var columnValues = this.columnForCell(cellIndex);
+  return this.lineBlockFor(symbol, columnValues);
 }
 
 Game.prototype.diagonalsBlockFor = function(symbol, cellIndex) {
   if (!this.cellOnDiagonal(cellIndex)) return 0; 
   
   var diagonalsValues = this.diagonalsForCell(cellIndex);
+  var that = this;
   return diagonalsValues.reduce(function(count, diagonalValues) {
     if (diagonalValues == null) return count;
-    var notAlreadyBlocked = diagonalValues.every(function(cellValue){
-      return cellValue != symbol;
-    });
-    var blocks = diagonalValues.some(function(cellValue) {
-      return cellValue != symbol && cellValue != null;
-    });
-    if (notAlreadyBlocked && blocks) count += 1;
+    count += that.lineBlockFor(symbol, diagonalValues);
     return count;
   }, 0);
 }
 
+Game.prototype.lineBlockFor = function(symbol, lineValues) {
+  var notAlreadyBlocked = lineValues.every(function(cellValue){
+    return cellValue != symbol;
+  });
+  var blockCount = lineValues.reduce(function(count, cellValue) {
+    if (cellValue != symbol && cellValue != null) count += 1;
+    return count;
+  }, 0);
+  return notAlreadyBlocked ? blockCount : 0;
+}
+
+Game.prototype.lineOpenFor = function(symbol, lineValues) {
+  var lineOpen = lineValues.every(function(cellValue) {
+    return cellValue == symbol || cellValue == null;
+  });
+  var symbolCount = lineValues.reduce(function(count, cellValue) {
+    if (cellValue == symbol) count += 1;
+    return count;
+  }, 0); 
+  return lineOpen ? (symbolCount || 1) : 0;
+}
+
 Game.prototype.rowOpenFor = function(symbol, cellIndex) {
-  var open = this.rowForCell(cellIndex).every(function(cellValue) {
-    return cellValue == symbol || cellValue == null; 
-  }); 
-  return open ? 1 : 0;
+  var rowValues = this.rowForCell(cellIndex);
+  return this.lineOpenFor(symbol, rowValues);
 }
 
 Game.prototype.columnOpenFor = function(symbol, cellIndex) {
-  var open = this.columnForCell(cellIndex).every(function(cellValue) {
-    return cellValue == symbol || cellValue == null;
-  });
-  return open ? 1 : 0;
+  var columnValues = this.columnForCell(cellIndex);
+  return this.lineOpenFor(symbol, columnValues);
 }
 
 Game.prototype.diagonalsOpenFor = function(symbol, cellIndex) {
   if (!this.cellOnDiagonal(cellIndex)) return 0; 
   
   var diagonalsValues = this.diagonalsForCell(cellIndex);
+  var that = this;
   return diagonalsValues.reduce(function(count, diagonalValues) {
     if (diagonalValues == null) return count;
-    var diagonalOpen = diagonalValues.every(function(cellValue) {
-      return cellValue == symbol || cellValue == null;
-    });
-    if (diagonalOpen) count += 1;
+    count += that.lineOpenFor(symbol, diagonalValues);
     return count;
   }, 0);
+}
+
+Game.prototype.lineOpenFor = function(symbol, lineValues) {
+  var lineOpen = lineValues.every(function(cellValue) {
+    return cellValue == symbol || cellValue == null;
+  });
+  var symbolCount = lineValues.reduce(function(count, cellValue) {
+    if (cellValue == symbol) count += 1;
+    return count;
+  }, 0); 
+  return lineOpen ? (symbolCount || 1) : 0;
 }
 
 Game.prototype.rowForCell = function(cellIndex) {
